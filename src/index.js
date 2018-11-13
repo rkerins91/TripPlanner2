37c664557eac11ece3a83a4a2b852f75b038078a
Willy Mello <cwmello165@gmail.com>
2018-11-13 16:21:38 -0500

    map working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('hello');
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoiY2hpbGx5Y2hlbGxvIiwiYSI6ImNqb2c4Y29rZjBjZmszcHBoeDk4andxa2oifQ.v2tfKOPNwJqF2ir5utTCIg";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+var marker = new mapboxgl.Marker()
+  .setLngLat([-74.009151, 40.705086])
+  .addTo(map)
